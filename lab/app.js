f2d9d003d438e38605f4d6240f24c47b3eac537b
Farrah Isabelle <farrahrios@pursuit.org>
2020-01-11 17:04:27 -0500

    boolean works

---
+++ lab/app.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const cors = require("cors")
+const port = 3000
+const app = express()
+app.use(cors())
+
+const animalObject = ["zebra","lion" ,"dog"]
+
+
+
+
+const isAnimal =(req,res,next)=>{
+        if(animalObject.includes(req.params.animalid)){ 
+            console.log("test")
+   }else{
+        res.json("false")
+    }
+next()
+}
+
+app.use("/animal/:animalid",isAnimal)
+
+app.get("/animal",(req,res)=>{
+res.json()
+})
+
+app.get("/animal/:animalid",(req,res)=>{
+    res.json("true")
+    })
+
+
+
+
+
+app.listen(port,()=>{
+console.log("listening to",port)
+})
+
